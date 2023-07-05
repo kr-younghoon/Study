5607fec2c2c4d58f34f1c2d4c1662c4c99acbe8e
kr-younghoon <sullanta0802@gmail.com>
2023-07-06 02:31:03 +0900

    52. "State"와 함께 일하기

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-    let title = props.title;
+    
+    const [title, setTitle] = useState(props.title);
+
 
     const clickHandler = () => {
-        title = 'Updated!';
+        setTitle('Updated!');
+        console.log(title);
     };
 
     return (
@@ -17,7 +20,7 @@ const ExpenseItem = (props) => {
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
-            </div>
+            </div> 
             <button onClick={clickHandler}>Change title</button>
         </Card>
     );
