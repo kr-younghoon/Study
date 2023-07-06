7e1614a89917a7634f63e1d7bb6586246ed26325
kr-younghoon <sullanta0802@gmail.com>
2023-07-06 15:19:33 +0900

    63. State 끌어올리기

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
-    
     const [title, setTitle] = useState(props.title);
     console.log('ExpenseItem evaluated by React');
-
 
     const clickHandler = () => {
         setTitle('Updated!');
@@ -21,7 +19,7 @@ const ExpenseItem = (props) => {
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
-            </div> 
+            </div>
             <button onClick={clickHandler}>Change title</button>
         </Card>
     );
