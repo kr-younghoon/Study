b39c77dbb5514a0bce9123aa1132b83bf2e3f439
kr-younghoon <sullanta0802@gmail.com>
2023-07-18 18:18:48 +0900

    relocation

---
+++ main/src/components/UI/Card.js
@@ -0,0 +1,19 @@
+// import './Card.css';
+
+// function Card(props) {
+//     const classes = 'card ' + props.className;
+
+//     return <div className="{classes}">{props.children}</div>; // children -> 에약어
+// }
+
+// export default Card;
+import React from 'react';
+import './Card.css';
+
+const Card = (props) => {
+    const classes = 'card ' + props.className;
+
+    return <div className={classes}>{props.children}</div>;
+};
+
+export default Card;
